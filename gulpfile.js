475798a78221afd55f2252ca6e136f7da50e7d8a
lhas <luizhrqas@gmail.com>
2016-07-22 12:52:07 -0300

    atualizacao

--- gulpfile.js
+++ gulpfile.js
@@ -80,8 +80,8 @@ gulp.task('images', () => {
 });
 
 gulp.task('fonts', () => {
-  return gulp.src(require('main-bower-files')('**/*.{eot,svg,ttf,woff,woff2}', function (err) {})
-    .concat('app/fonts/**/*'))
+  return gulp.src('app/fonts/**/*.{eot,svg,ttf,woff,woff2}', function (err) {})
+    // .concat('app/fonts/**/*'))
     .pipe(gulp.dest('.tmp/fonts'))
     .pipe(gulp.dest('dist/fonts'));
 });
